e3ba7a716cb53ab9c88bb10b8fb24885180a618d
umbrel <s@umbrellab.com>
2018-04-10 19:45:09 +1000

    Add tx type for signing requests

--- index.js
+++ index.js
@@ -4,62 +4,77 @@ const StellarSdk = require('stellar-sdk');
 const _ = require('lodash');
 const uuid = require('uuid');
 
-if (process.env.NODE_ENV === 'production') {
-  StellarSdk.Network.usePublicNetwork();
-} else {
-  StellarSdk.Network.useTestNetwork();
-}
-
-let server = new StellarSdk.Server(process.env.HORIZON_URL || 'https://horizon.stellar.org');
-
 const StellarSign = {
 
   setHorizonUrl: (url) => {
-      this.url = url;
-      server = new StellarSdk.Server(this.url);
-      return this;
+    StellarSign.url = url;
+    return StellarSign;
+  },
+
+  useNetwork: (networkPassphrase) => {
+    StellarSign.networkPassphrase = networkPassphrase;
+    return StellarSign;
+  },
+
+  getServer() {
+    if (!StellarSign.server) {
+      StellarSign.server = new StellarSdk.Server(StellarSign.horizonUrl || process.env.HORIZON_URL || 'https://horizon.stellar.org');
+      StellarSdk.Network.use(new StellarSdk.Network(StellarSign.networkPassphrase || (
+        process.env.NODE_ENV === 'production' ? StellarSdk.Networks.PUBLIC : StellarSdk.Networks.TESTNET
+      )));
+    }
+    return StellarSign.server;
   },
 
   /**
-   * Toggle home_domain validation off
-   * Can be useful for testing
    *
-   * @returns {*}
+   * @param xdr
+   * @param guid
+   * @param type
+   * @returns Array
    */
-  skipHomeDomainValidation: () => {
-      this._skipHomeDomainValidation = true;
-      return this;
+  encodeXdr: (xdr, guid, type = 'op') => {
+    let result = [];
+    for (let i = 0; i < Math.ceil(xdr.length / 64); i++) {
+      const chunk = xdr.slice(i * 64, (i + 1) * 64);
+      result.push({
+        name: `srv1:${type}:${guid}:${i}`,
+        value: chunk,
+      });
+      result.push({
+        name: `srv1:${type}:${guid}:${i}`,
+        value: null,
+      });
+    }
+    return result;
   },
 
-  useTestNetwork: () => {
-      StellarSdk.Network.useTestNetwork();
-      return this;
-  },
-
-  usePublicNetwork: () => {
-      StellarSdk.Network.usePublicNetwork();
-      return this;
+  resolveAccount: (destAddress) => {
+    return new Promise(function (resolve, reject) {
+      !!destAddress.account_id
+        ? resolve(destAddress)
+        : StellarSdk.FederationServer.resolve(destAddress)
+          .then(result => {
+            resolve(result);
+          })
+          .catch(err => {
+            reject(err);
+          });
+    });
   },
 
   /**
    *
-   * @param Keypair srcKeydebug
-   * @param string destAddress
-   * @param Buffer xdr
+   * @param srcKeypair
+   * @param destAddress
+   * @param xdr
    */
   requestOperation: (srcKeypair, destAddress, xdr) => {
-    let fedRecord = null;
-
-    return new Promise(function(resolve, reject) {
-        !!destAddress.account_id
-            ? resolve(destAddress)
-            : resolve(StellarSdk.FederationServer.resolve(destAddress));
-    })
-      .then((result) => {
-          fedRecord = result;
-          return server.loadAccount(srcKeypair.publicKey())
-      })
-      .then(sourceAccount => {
+    return Promise.all([
+      StellarSign.resolveAccount(destAddress),
+      StellarSign.getServer().loadAccount(srcKeypair.publicKey())
+    ])
+      .then(([fedRecord, sourceAccount]) => {
         const options = (fedRecord.memo ? {memo: new StellarSdk.Memo(fedRecord.memo_type, fedRecord.memo)} : {});
         const builder = new StellarSdk.TransactionBuilder(sourceAccount, options);
 
@@ -70,29 +85,41 @@ const StellarSign = {
             amount: '0.0000001'
           }));
 
-        const guid = uuid.v4();
-
-        for (let i = 0; i < Math.ceil(xdr.length/64); i++) {
-          const chunk = xdr.slice(i*64, (i+1)*64);
-          builder
-            .addOperation(StellarSdk.Operation.manageData({
-              name: ('srv1:op:' + guid + ':' + i),
-              value: chunk,
-            }));
-        }
-
-        for (let i = 0; i < Math.ceil(xdr.length/64); i++) {
-          builder
-            .addOperation(StellarSdk.Operation.manageData({
-              name: ('srv1:op:' + guid + ':' + i),
-              value: '',
-            }));
-        }
+        StellarSign.encodeXdr(xdr, uuid.v4(), 'op').forEach((item) => {
+          builder.addOperation(StellarSdk.Operation.manageData(item));
+        });
 
         const tx = builder.build();
         tx.sign(srcKeypair);
 
-        return server.submitTransaction(tx);
+        return StellarSign.getServer().submitTransaction(tx);
+      });
+  },
+
+  requestTransaction: (srcKeypair, destAddress, xdr) => {
+    return Promise.all([
+      StellarSign.resolveAccount(destAddress),
+      StellarSign.getServer().loadAccount(srcKeypair.publicKey())
+    ])
+      .then(([fedRecord, sourceAccount]) => {
+        const options = (fedRecord.memo ? {memo: new StellarSdk.Memo(fedRecord.memo_type, fedRecord.memo)} : {});
+        const builder = new StellarSdk.TransactionBuilder(sourceAccount, options);
+
+        builder
+          .addOperation(StellarSdk.Operation.payment({
+            destination: fedRecord.account_id,
+            asset: StellarSdk.Asset.native(),
+            amount: '0.0000001'
+          }));
+
+        StellarSign.encodeXdr(xdr, uuid.v4(), 'tx').forEach((item) => {
+          builder.addOperation(StellarSdk.Operation.manageData(item));
+        });
+
+        const tx = builder.build();
+        tx.sign(srcKeypair);
+
+        return StellarSign.getServer().submitTransaction(tx);
       });
   },
 
@@ -101,72 +128,75 @@ const StellarSign = {
    * @returns {Promise<Array>}
    */
   parseTx: (tx) => {
-      const manageDataOps = _.filter(tx.operations, (item) => {
-          if (item.type !== 'manageData') {
-              return false;
-          }
-          const itemPieces = item.name.split(':');
-          return item.value.length > 0
-              && itemPieces[0] && itemPieces[0] === 'srv1'
-              && itemPieces[1] && itemPieces[1] === 'op'
-              && itemPieces[2] && itemPieces[3];
-      });
-
-      if (!manageDataOps.length) {
-          return Promise.reject(new Error('No relevant manageData operations found'));
+    const manageDataOps = _.filter(tx.operations, (item) => {
+      if (item.type !== 'manageData' || !item.value) {
+        return false;
       }
+      const itemPieces = item.name.split(':');
+      return itemPieces[0] && itemPieces[0] === 'srv1'
+        && itemPieces[1] && (itemPieces[1] === 'op' || itemPieces[1] === 'tx')
+        && itemPieces[2] && itemPieces[3];
+    });
 
-      const result = {};
-      _.each(manageDataOps, (item) => {
-          const itemPieces = item.name.split(':');
-          if (!result[itemPieces[2]]) {
-              result[itemPieces[2]] = { parts: [], bufferSize: 0 };
-          }
-          result[itemPieces[2]].parts[itemPieces[3]] = item.value;
-          result[itemPieces[2]].bufferSize += item.value.length;
-          result[itemPieces[2]].version = itemPieces[0];
-          result[itemPieces[2]].type = itemPieces[1];
-      });
+    if (!manageDataOps.length) {
+      return Promise.reject(new Error('No relevant manageData operations found'));
+    }
 
-      return Promise.resolve(_.map(result, (item) => {
-          return {
-              version: item.version,
-              type: item.type,
-              body: Buffer.concat(item.parts, item.bufferSize).toString('base64'),
-          }
-      }));
+    const result = {};
+    _.each(manageDataOps, (item) => {
+      const itemPieces = item.name.split(':');
+      if (!result[itemPieces[2]]) {
+        result[itemPieces[2]] = {parts: [], bufferSize: 0};
+      }
+      result[itemPieces[2]].parts[itemPieces[3]] = item.value;
+      result[itemPieces[2]].bufferSize += item.value.length;
+      result[itemPieces[2]].version = itemPieces[0];
+      result[itemPieces[2]].type = itemPieces[1];
+    });
+
+    return Promise.resolve(_.map(result, (item) => {
+      return {
+        version: item.version,
+        type: item.type,
+        body: Buffer.concat(item.parts, item.bufferSize).toString('base64'),
+      }
+    }));
   },
 
-  decodeXDR: (xdr) => {
+  decodeXDR: (xdr, skipSenderDomainValidation = false) => {
     const tx = new StellarSdk.Transaction(xdr);
 
-    if (this._skipHomeDomainValidation) {
-        return StellarSign.parseTx(tx);
-    } else {
-        return server.loadAccount(tx.source)
-            .then(srcAccount => {
+    return StellarSign.getServer().loadAccount(tx.source)
+      .then(srcAccount => {
 
-                if (!srcAccount.home_domain) {
-                    return Promise.reject(new Error('Home domain not set on source account'));
-                }
+        if (skipSenderDomainValidation) {
+          return Promise.resolve(true);
+        }
 
-                return StellarSdk.StellarTomlResolver.resolve(srcAccount.home_domain)
-                    .catch(err => {
-                        return Promise.reject(new Error('stellar.toml placed on home domain is not found or invalid: ' + err.message))
-                    })
-                    .then(tomlObject => {
-                        if (!tomlObject.SIGNING_REQUEST_ACCOUNT || tomlObject.SIGNING_REQUEST_ACCOUNT !== tx.source) {
-                            return Promise.reject('SIGNING_REQUEST_ACCOUNT doesn\'t exist in stellar.toml or doesn\'t match the source account')
-                        }
+        if (!srcAccount.home_domain) {
+          return Promise.reject(new Error('Home domain not set on source account'));
+        }
 
-                        return StellarSign.parseTx(tx)
-                            .then((result) => {
-                                result.sender = srcAccount.home_domain;
-                                return result;
-                            })
-                    });
-            });
-    }
+        return StellarSdk.StellarTomlResolver.resolve(srcAccount.home_domain)
+          .then(tomlObject => {
+            if (!tomlObject.SIGNING_REQUEST_ACCOUNT || tomlObject.SIGNING_REQUEST_ACCOUNT !== tx.source) {
+              return Promise.reject('SIGNING_REQUEST_ACCOUNT doesn\'t exist in stellar.toml or doesn\'t match the source account')
+            }
+          })
+          .catch(err => {
+            return Promise.reject(new Error('stellar.toml placed on home domain is not found or invalid: ' + err.message))
+          })
+          .then(() => {
+            return StellarSign.parseTx(tx)
+          })
+          .then((result) => {
+            result.sender = srcAccount.home_domain;
+            return result;
+          });
+      })
+      .catch(err => {
+        return Promise.reject(new Error('Decoding XDR failed: ' + err.message))
+      });
   }
 };
 
